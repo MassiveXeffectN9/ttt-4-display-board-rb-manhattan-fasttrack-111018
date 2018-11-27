cc38878d1ab8d5c8b93a78587d6eccc50793ede9
Jeffrey Galvan <jgalvan09@gmail.com>
2018-11-27 17:16:52 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,114 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+puts "   |   |   "  
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end
+
+board = [" "," "," "," ","X"," "," "," "," "]
+
+def display_board(board)
+  rows ="x"
+puts "   |   |   "  
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end
+
+board = ["O"," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+puts " o |   |   "  
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end
+
+board = ["O"," "," "," ","X"," "," "," "," "]
+
+def display_board(board)
+puts "   |   |   "  
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end
+
+board = ["X","X","X"," "," "," "," "," "," "]
+
+def display_board(board)
+puts "   |   |   "  
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end
+
+board = [" "," "," "," "," "," ","O","O","O"]
+
+def display_board(board)
+puts "   |   |   "  
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end
+
+board = ["X"," "," "," ","X"," "," "," ","X"]
+
+def display_board(board)
+puts "   |   |   "  
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end
+
+board = [" "," ","O"," ","O"," ","O"," "," "]
+
+def display_board(board)
+puts "   |   |   "  
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+puts "   |   |   "  
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+puts "   |   |   "  
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+puts "   |   |   "  
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end
+
